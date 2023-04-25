0894dff103f60234131e89f837612a543e584108
bzd <artur@codecompass.pl>
2023-04-25 23:40:20 +0200

    Db connection

--- CardsDb/Program.cs
+++ CardsDb/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using CardsDb.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<CardContext>(opt => opt.UseInMemoryDatabase("CardList"));
+//builder.Services.AddDbContext<CardContext>(opt => opt.UseInMemoryDatabase("CardList"));
+builder.Services.AddDbContext<CardContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("CardsDatabase")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
